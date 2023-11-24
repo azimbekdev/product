3fd674b87d065bb1905036cf576082286de699d8
azimbekdev <azimbekmederbekov@gmail.com>
2023-11-24 14:11:50 -0100

    new product

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn-three"),
+    body = document.querySelector('body');
+
+btn.addEventListener('click', () => {
+    if (body.className === '') {
+        body.classList.add('dark')
+    } else body.className = ''
+})
+
